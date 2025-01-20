1446afa72569e84ffdc4282bcfc00d558c6a1faa
andrew000 <11490628+andrew000@users.noreply.github.com>
2025-01-20 17:08:20 +0200

    Fix not passing redis to `start_cmd` in `start_cmd_with_deep_link`

--- app/bot/handlers/cmds/start.py
+++ app/bot/handlers/cmds/start.py
@@ -20,13 +20,13 @@ logger = logging.getLogger(__name__)
 
 
 @router.message(CommandStart(deep_link=True))
-async def start_cmd_with_deep_link(msg: Message, command: CommandObject, i18n: I18nContext) -> None:
+async def start_cmd_with_deep_link(msg: Message, command: CommandObject, i18n: I18nContext, redis: Redis) -> None:
     args = command.args.split() if command.args else []
     deep_link = args[0]
 
     logger.info("User %s started bot with deeplink: %s", msg.from_user.id, deep_link)
 
-    await start_cmd(msg, i18n)
+    await start_cmd(msg, i18n, redis)
 
 
 @router.message(CommandStart(deep_link=False))  # Deeplink in False will not work as expected
